52146c8f07d08c413712d79d6ddec8fb6f1b1e5e
Kevin Chan <kevinykchan@gmail.com>
2020-08-27 22:31:16 -0400

    Initial Commit

---
+++ Project4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Kevin Chan on 8/27/20.
+//  Copyright © 2020 Visionary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
